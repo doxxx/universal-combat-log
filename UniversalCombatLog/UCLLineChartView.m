dae52fdebc9df4b0ff85744be50cb9a73cd3ce62
Gordon Tyler <gordon@doxxx.net>
2012-08-07 08:02:15 -0400

    Draw only axes in line chart when no data present.

--- UniversalCombatLog/UCLLineChartView.m
+++ UniversalCombatLog/UCLLineChartView.m
@@ -52,33 +52,39 @@
     CGRect bounds = [self bounds];
     CGFloat chartWidth = bounds.size.width - (LINSET + RINSET);
     CGFloat chartHeight = bounds.size.height - YINSET*2;
-    CGFloat xScale = chartWidth / ([self.data count] - 1);
-    CGFloat yScale = chartHeight / [_maxValue doubleValue];
-    
+
     // Flip co-ordinate system to bottom left going up and right.
     CGContextTranslateCTM(c, 0, bounds.size.height);
     CGContextScaleCTM(c, 1, -1);
     
-    // Draw data line.
-    CGContextSetRGBStrokeColor(c, 0, 1, 0, 1);
-    CGContextSetLineWidth(c, 2);
-    CGContextSetLineJoin(c, kCGLineJoinRound);
-    CGContextSetLineCap(c, kCGLineCapRound);
-    
-    int count = 0;
-    for (NSNumber* value in self.data) {
-        CGFloat x = LINSET + xScale * count;
-        CGFloat y = YINSET + [value doubleValue] * yScale;
-        if (count == 0) {
-            CGContextMoveToPoint(c, x, y);
+    CGFloat xScale = 1;
+    CGFloat yScale = 1;
+
+    if (self.data != nil) {
+        xScale = chartWidth / ([self.data count] - 1);
+        yScale = chartHeight / [_maxValue doubleValue];
+        
+        // Draw data line.
+        CGContextSetRGBStrokeColor(c, 0, 1, 0, 1);
+        CGContextSetLineWidth(c, 2);
+        CGContextSetLineJoin(c, kCGLineJoinRound);
+        CGContextSetLineCap(c, kCGLineCapRound);
+        
+        int count = 0;
+        for (NSNumber* value in self.data) {
+            CGFloat x = LINSET + xScale * count;
+            CGFloat y = YINSET + [value doubleValue] * yScale;
+            if (count == 0) {
+                CGContextMoveToPoint(c, x, y);
+            }
+            else {
+                CGContextAddLineToPoint(c, x, y);
+            }
+            count++;
         }
-        else {
-            CGContextAddLineToPoint(c, x, y);
-        }
-        count++;
+        
+        CGContextStrokePath(c);
     }
-    
-    CGContextStrokePath(c);
     
     // Draw axes.
     CGContextSetRGBStrokeColor(c, 0.8, 0.8, 0.8, 1);
@@ -93,45 +99,46 @@
     
     UIFont* axisMarkerFont = [UIFont systemFontOfSize:10];
     
-    // Draw markers on axes.
-    NSUInteger yMarkerCount = floor([_maxValue doubleValue] / self.yInterval);
-    for (NSUInteger i = 1; i <= yMarkerCount; i++) {
-        CGFloat y = YINSET + (i * self.yInterval) * yScale;
-        CGContextMoveToPoint(c, LINSET, y);
-        CGContextAddLineToPoint(c, LINSET - MARKER_LENGTH, y);
-        CGContextStrokePath(c);
+    if (_maxValue != nil) {
+        // Draw markers on axes.
+        NSUInteger yMarkerCount = floor([_maxValue doubleValue] / self.yInterval);
+        for (NSUInteger i = 1; i <= yMarkerCount; i++) {
+            CGFloat y = YINSET + (i * self.yInterval) * yScale;
+            CGContextMoveToPoint(c, LINSET, y);
+            CGContextAddLineToPoint(c, LINSET - MARKER_LENGTH, y);
+            CGContextStrokePath(c);
+        }
+        
+        NSUInteger xMarkerCount = floor([self.data count] / self.xInterval);
+        for (NSUInteger i = 1; i <= xMarkerCount; i++) {
+            CGFloat x = YINSET + (i * self.xInterval) * xScale;
+            CGContextMoveToPoint(c, x, YINSET);
+            CGContextAddLineToPoint(c, x, YINSET - MARKER_LENGTH);
+            CGContextStrokePath(c);
+        }
+
+        // Reverse CTM so that text is not drawn upside down.
+        CGContextScaleCTM(c, 1, -1);
+        CGContextTranslateCTM(c, 0, -bounds.size.height);
+        
+        // Draw axis marker labels.
+        CGContextSetRGBFillColor(c, 0.8, 0.8, 0.8, 1);
+        for (NSUInteger i = 1; i <= yMarkerCount; i++) {
+            CGFloat y = bounds.size.height - (YINSET + (i * self.yInterval) * yScale);
+            NSString* markerLabel = [NSString stringWithFormat:@"%0.0f", (i * self.yInterval)];
+            CGSize labelSize = [markerLabel sizeWithFont:axisMarkerFont];
+            [markerLabel drawAtPoint:CGPointMake(LINSET - MARKER_LENGTH - labelSize.width, y - labelSize.height/2) 
+                            withFont:axisMarkerFont];
+        }
+        
+        for (NSUInteger i = 1; i <= xMarkerCount; i++) {
+            CGFloat x = YINSET + (i * self.xInterval) * xScale;
+            NSString* markerLabel = [NSString stringWithFormat:@"%0.0f", (i * self.xInterval)];
+            CGSize labelSize = [markerLabel sizeWithFont:axisMarkerFont];
+            [markerLabel drawAtPoint:CGPointMake(x - labelSize.width/2, bounds.size.height - YINSET + MARKER_LENGTH) 
+                            withFont:axisMarkerFont];
+        }
     }
-    
-    NSUInteger xMarkerCount = floor([self.data count] / self.xInterval);
-    for (NSUInteger i = 1; i <= xMarkerCount; i++) {
-        CGFloat x = YINSET + (i * self.xInterval) * xScale;
-        CGContextMoveToPoint(c, x, YINSET);
-        CGContextAddLineToPoint(c, x, YINSET - MARKER_LENGTH);
-        CGContextStrokePath(c);
-    }
-    
-    // Reverse CTM so that text is not drawn upside down.
-    CGContextScaleCTM(c, 1, -1);
-    CGContextTranslateCTM(c, 0, -bounds.size.height);
-    
-    // Draw axis marker labels.
-    CGContextSetRGBFillColor(c, 0.8, 0.8, 0.8, 1);
-    for (NSUInteger i = 1; i <= yMarkerCount; i++) {
-        CGFloat y = bounds.size.height - (YINSET + (i * self.yInterval) * yScale);
-        NSString* markerLabel = [NSString stringWithFormat:@"%0.0f", (i * self.yInterval)];
-        CGSize labelSize = [markerLabel sizeWithFont:axisMarkerFont];
-        [markerLabel drawAtPoint:CGPointMake(LINSET - MARKER_LENGTH - labelSize.width, y - labelSize.height/2) 
-                        withFont:axisMarkerFont];
-    }
-    
-    for (NSUInteger i = 1; i <= xMarkerCount; i++) {
-        CGFloat x = YINSET + (i * self.xInterval) * xScale;
-        NSString* markerLabel = [NSString stringWithFormat:@"%0.0f", (i * self.xInterval)];
-        CGSize labelSize = [markerLabel sizeWithFont:axisMarkerFont];
-        [markerLabel drawAtPoint:CGPointMake(x - labelSize.width/2, bounds.size.height - YINSET + MARKER_LENGTH) 
-                        withFont:axisMarkerFont];
-    }
-    
 }
 
 @end

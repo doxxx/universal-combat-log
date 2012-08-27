885d808bcddb5b4631ea0921ecc204d9d4f6c02d
Gordon Tyler <gordon@doxxx.net>
2012-08-27 17:39:52 -0400

    Use better name for popover button.

--- UniversalCombatLog/UCLActorViewController.m
+++ UniversalCombatLog/UCLActorViewController.m
@@ -151,7 +151,7 @@
 
 - (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
 {
-    barButtonItem.title = NSLocalizedString(@"Fights", @"Fights");
+    barButtonItem.title = NSLocalizedString(@"Logs", @"Logs");
     [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
     _masterPopoverController = popoverController;
 }
